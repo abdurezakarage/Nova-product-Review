333bd532fd1ac6669cd63d19ea0f5f09c4f87c69
abdurezakarage <abdurezakhwre@gmail.com>
2024-12-04 19:09:59 +0300

    updaed header & product page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,50 @@
 import React, { useState } from "react";
-import { Image } from "@mantine/core";
-import { Link } from "react-router-dom"; 
-import "./Header.css"; // 
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false); 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
   };
 
   return (
     <div className="header-container">
+      {/* Logo */}
       <div className="logo">
-        <Link to="/">
-          <Image
+        <Link to="/" onClick={closeMenu}>
+          <img
             src="https://www.nova-infotech.com/assets/img/logo.png"
             alt="Nova Infotech Logo"
+            className="logo-image"
           />
         </Link>
       </div>
-      
-      
+
+      {/* Navigation Links */}
       <div className={`links ${isMenuOpen ? "open" : ""}`}>
         <div className="button-container">
-          <Link to="/" className="nav-link">
+          <Link to="/" className="nav-link" onClick={closeMenu}>
             Home
           </Link>
-          <Link to="/create-product" className="nav-link">
+          <Link to="/create-product" className="nav-link" onClick={closeMenu}>
             Create Product
           </Link>
         </div>
       </div>
 
-      {/* button for mobile */}
-      <div className="icon" onClick={toggleMenu}>
+      {/* Expand/Collapse Icon */}
+      <div
+        className="icon"
+        onClick={toggleMenu}
+        role="button"
+        aria-label="Toggle Menu"
+      >
         <span className="bar"></span>
         <span className="bar"></span>
         <span className="bar"></span>
